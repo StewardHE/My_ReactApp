4cd3339cf7901e22f7022d632759dd5244c0772e
Steward <bryanherreraestrada@gmail.com>
2022-11-22 15:30:53 -0600

    New update, added loading states

--- src/App.js
+++ src/App.js
@@ -15,15 +15,31 @@ function GithubUser({ followers_url, events_url }) {
 function App() {
   // Here we use useState
   const [data, setData] = useState(null);
+
+  // this is for add a loading state and a error state
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   // this is for fetch the data from the api
   useEffect(() => {
+    setLoading(true);
     fetch(
       `https://api.github.com/`
       )
       
+      //
       .then((response => response.json()))
-      .then(setData);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
+  // if loading is true
+  if (loading) return <h1>Loading</h1>
+  // if there's a error show it (json format)
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  // if there's no data say null
+  if (!data) return null;
+
   // if there are some data, Show it
   if(data) 
   // shows the followers data in the API
