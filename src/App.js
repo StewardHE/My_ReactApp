7cc1085bb2f1763746fa3b0aaf00b809b365075d
Steward <bryanherreraestrada@gmail.com>
2022-11-22 17:18:49 -0600

    New Update, i changed the code to fetch data with GraphQL

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-// Fetch data from Github User API
-function GithubUser({ followers_url, events_url }) {
-  return(
+// query
+const query = `
+query {
+  allLifts {
+    name
+    elevationGain
+    status
+  }
+}
+`;
+
+// options to the query
+const opts = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ query })
+};
+
+// Fetch data from lift API
+function Lift({ name, elevationGain, status }) {
+  return (
     <div>
-      <h1>{ followers_url }</h1>
-      <p>{ events_url }</p>
+      <h1>{name}</h1>
+      <p>
+        {elevationGain} {status}
+      </p>
     </div>
   );
 }
+
 
 // this component is to fetch data from the github Api
 function App() {
   // Here we use useState
   const [data, setData] = useState(null);
 
-  // this is for add a loading state and a error state
+  // this is for add a loading state before show the data and a error state
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -24,13 +45,16 @@ function App() {
   useEffect(() => {
     setLoading(true);
     fetch(
-      `https://api.github.com/`
+      `https://snowtooth.moonhighway.com/`,
+      opts
       )
       
       //
       .then((response => response.json()))
       .then(setData)
+      // set loading false
       .then(() => setLoading(false))
+      // catch the error
       .catch(setError);
   }, []);
   // if loading is true
@@ -39,20 +63,23 @@ function App() {
   if (error) return <pre>{JSON.stringify(error)}</pre>;
   // if there's no data say null
   if (!data) return null;
+  // send the date to the console
+  console.log(data, "DATA!!");
 
   // if there are some data, Show it
   if(data) 
-  // shows the followers data in the API
-    return (
-      <GithubUser 
-      // shows the followers on the API
-      followers_url={data.followers_url} 
-      // shows the events on the api
-      events_url={data.events_url}
-      />
-      
-    )
-  return <h1>Data</h1>;
+  // Shows al the lifts from the API 
+  return (
+    <div>
+      {data.data.allLifts.map((lift) => (
+        <Lift
+          name={lift.name}
+          elevationGain={lift.elevationGain}
+          status={lift.status}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
