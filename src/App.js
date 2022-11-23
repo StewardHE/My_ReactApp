28ff233877f7e29ab14003ab5816abfe2d67f520
Steward <bryanherreraestrada@gmail.com>
2022-11-23 16:26:03 -0600

    New update, i changed the code to start using ReactRouter, in this new update i already set up 3 routes: Home, About, Contact

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
 import './App.css';
 
-// data
-const tahoe_peaks = [
-  { name: "Freel", elevation: 10929 },
-  { name: "Monument", elevation: 12902},
-  { name: "pyramid", elevation: 345677},
-  { name: "Tallac",  elevation: 73821}
-];
-
-// if we have data, show it, else, print empty
-function List({data, renderItem, renderEmpty}) {
-  return !data.length ? renderEmpty : <ul>{data.map((item) => (
-    <li key={item.name}>
-      {renderItem(item)}
-    </li>
-  ))}</ul>
+// Home component and page using reactRouter.
+function Home() {
+  return (
+    <div>
+      <h1>My Website</h1>
+    </div>
+  );
 }
 
-function App() {
- return (
-  // render the list
-  <div>
-    <List 
-      data={tahoe_peaks} 
-      renderEmpty={<p>This is empty</p>} 
-      renderItem={item => (
-        <>
-        {item.name} - {item.elevation} ft.
-        </>
-  )}
-    />
-  </div>
- );
+// About component and page using reactROuter
+export function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+}
+// Contact component and page using reactROuter
+export function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+    </div>
+  );
 }
 
-export default App;
+export function App() {
+ return <Home />;
+}
+
+
+
